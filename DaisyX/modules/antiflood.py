b8e52f620cbc45e801285c400ccb88c02de3cbda
ChankitSaini <75587598+ChankitSaini@users.noreply.github.com>
2021-04-17 15:53:36 +0530

    Update antiflood.py

--- DaisyX/modules/antiflood.py
+++ DaisyX/modules/antiflood.py
@@ -1,4 +1,4 @@
-# This file is part of Daisy (Telegram Bot)
+# This file is part of Queer ♕ (Telegram Bot)
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -316,7 +316,9 @@ Antiflood allows you to take action on users that send more than x messages in a
 
 <b>Admins only:</b>
 - /antiflood: Gives you current configuration of antiflood in the chat
+
 - /antiflood off: Disables Antiflood
+
 - /setflood (limit): Sets flood limit
 
 Replace (limit) with any integer, should be less than 200. When setting up, Daisy would ask you to send expiration time, if you dont understand what this expiration time for? User who sends specified limit of messages consecutively within this TIME, would be kicked, banned whatever the action is. if you dont want this TIME, wants to take action against those who exceeds specified limit without mattering TIME INTERVAL between the messages. you can reply to question with 0
