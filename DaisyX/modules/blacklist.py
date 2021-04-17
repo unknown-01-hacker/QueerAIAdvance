76e7aa8c288149b3df0233fe88442f9c6aee132a
ChankitSaini <75587598+ChankitSaini@users.noreply.github.com>
2021-04-17 16:46:25 +0530

    Update blacklist.py

--- DaisyX/modules/blacklist.py
+++ DaisyX/modules/blacklist.py
@@ -200,7 +200,7 @@ async def _(event):
 
 
 __help__ = """
-<b> Daisy's filters are the blacklist too </b>
+<b> Queer ♕'s filters are the blacklist too </b>
  - /addfilter [trigger] Select action: blacklists the trigger
  - /delfilter [trigger] : stop blacklisting a certain blacklist trigger
  - /filters: list all active blacklist filters
