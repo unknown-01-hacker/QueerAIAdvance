cc61c749e430fad7fddc8f2221505bc332963899
ChankitSaini <75587598+ChankitSaini@users.noreply.github.com>
2021-04-19 03:01:42 +0530

    Update ForceSubscribe.py

--- DaisyX/modules/ForceSubscribe.py
+++ DaisyX/modules/ForceSubscribe.py
@@ -194,16 +194,16 @@ def config(client, message):
 
 
 __help__ = """
-*Force Subscribe*.
+<b>Force Subscribe</b>
 ✪ Queer ♕ can mute members who are not subscribed your channel until they subscribe
 ✪ When enabled I will mute unsubscribed members and show them a unmute button. When they pressed the button I will unmute them
 
-*Setup*
+<b>Setup</b>
 
 1) First of all add me in the group as admin with ban users permission and in the channel as admin.
 Note: Only creator of the group can setup me and i will not allow force subscribe again if not done so.
  
-*Commmands*
+<b>Commmands</b>
  - /forcesubscribe - To get the current settings.
  - /forcesubscribe no/off/disable - To turn of ForceSubscribe.
  - /forcesubscribe {channel username} - To turn on and setup the channel.
