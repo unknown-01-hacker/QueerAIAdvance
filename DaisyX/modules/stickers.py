2856b1660ffd77562460574daf7e537d24427ddd
ChankitSaini <75587598+ChankitSaini@users.noreply.github.com>
2021-04-18 14:35:07 +0530

    Update stickers.py

--- DaisyX/modules/stickers.py
+++ DaisyX/modules/stickers.py
@@ -119,7 +119,7 @@ def find_instance(items, class_or_tuple):
     return None
 
 
-@QueerAIBot(pattern="^/searchsticker (.*)")
+@DaisyXBot(pattern="^/searchsticker (.*)")
 async def _(event):
     input_str = event.pattern_match.group(1)
     combot_stickers_url = "https://combot.org/telegram/stickers?q="
